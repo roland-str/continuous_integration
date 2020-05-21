644a415395ed91ec163c6c8850c662b4f4949feb
Evgeniy Tkachenko <evgentka@gmail.com>
2020-05-21 20:48:37 +0700

    handle warnings

--- server/src/main/java/ru/sbtqa/Greeter.java
+++ server/src/main/java/ru/sbtqa/Greeter.java
@@ -6,15 +6,18 @@ package ru.sbtqa;
 public class Greeter {
 
   /**
-   * Constructor of Greeter class
+   * Constructor of Greeter class.
    */
   public Greeter() {
 
   }
 
 
-
-
+  /**
+   * method for greaating a preson.
+   * @param name - name of a person
+   * @return - greeeting
+   */
   public final String greet(final String name) {
     return String.format("Hello, %s!", name);
   }
